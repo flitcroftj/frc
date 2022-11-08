1d25f923ce6b691a4173e26241de124d418c8af2
flitcroftj <jflitcroft14@gmail.com>
2022-11-07 20:24:48 -0600

    Added drive by encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -12,9 +12,6 @@ import java.util.concurrent.TimeUnit;
 @TeleOp
 public class MecanumTeleOp extends LinearOpMode {
 
-    public static void move_up() {
-
-    }
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -24,8 +21,12 @@ public class MecanumTeleOp extends LinearOpMode {
         DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");//motor three
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");// motor zero
         DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");//motor two
-        Servo testServo = hardwareMap.servo.get("servo0");
 
+        // Setting up the claw on the robot
+        Servo clawLeft = hardwareMap.servo.get("servo0");
+        Servo clawRight = hardwareMap.servo.get("servo1");
+        clawLeft.scaleRange(0.0, 1.0); //TODO Set the range that we can set the claws to
+        clawRight.scaleRange(0.0, 1.0);
 
         // Reverse the right side motors
         // Reverse left motors if you are using NeveRests
@@ -42,14 +43,10 @@ public class MecanumTeleOp extends LinearOpMode {
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
             double ry = gamepad1.right_stick_y;
+
             double lt = gamepad1.left_trigger;
-            boolean move_forward = gamepad1.dpad_up;
-            boolean move_right = gamepad1.dpad_right;
-            boolean move_left = gamepad1.dpad_left;
-            boolean move_backward = gamepad1.dpad_down;
-            // adj=adj+.01*rt;
-            // adj=adj-.01*lt;
-            // x=x+adj;
+            boolean open_claw = gamepad1.dpad_up;
+            boolean close_claw = gamepad1.dpad_down;
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
@@ -64,17 +61,15 @@ public class MecanumTeleOp extends LinearOpMode {
             motorBackLeft.setPower(backLeftPower);
             motorFrontRight.setPower(frontRightPower);
             motorBackRight.setPower(backRightPower);
-            //testServo.setDirection(Servo.Direction.);
-            if (move_forward == true) {
-                motorFrontLeft.setPower(1);
-                motorBackLeft.setPower(1);
-                motorFrontRight.setPower(1);
-                motorBackRight.setPower(1);
-                TimeUnit.SECONDS.sleep(5);
-            } //drive forward
-            //if (servo_down == true) {
-            //    testServo.setDirection(Servo.Direction.REVERSE);
-            //}
+
+            // We want to figure out what positions to set on the claw for open and close operations
+            if (open_claw) {
+                clawLeft.setPosition(0.0);
+                clawRight.setPosition(1.0);
+            } else if (close_claw) {
+                clawLeft.setPosition(1.0);
+                clawRight.setPosition(0.0); 
+            }
         }
     }
 }
